a2998b2d7b54a75232f65e94102c9feaecf38548
kai <leonc4667@gmail.com>
2023-03-03 16:10:48 -0400

    a

--- scratch.dart
+++ scratch.dart
@@ -1,13 +1,12 @@
-import 'dart:io';
-
 void main() {
   performTasks();
 }
 
-void performTasks()  {
+void performTasks() async {
   task1();
-  task2();
-  // task3();
+  String task2Result = await task2();
+  print(task2());
+  task3(task2Result);
 }
 
 void task1() {
@@ -15,20 +14,20 @@ void task1() {
   print('Task 1 complete');
 }
 
-void task2()  {
+Future<String> task2() async {
   Duration threeSeconds = Duration(seconds: 3);
 
-  String result;
+  late String result;
 
-  Future.delayed(threeSeconds, (){
+  await Future.delayed(threeSeconds, () {
     result = 'task 2 data';
     print('Task 2 complete');
   });
 
-
+  return result;
 }
 
-void task3(String task2Data) {
+void task3(String task2data) {
   String result = 'task 3 data';
-  print('Task 3 complete  $task2Data');
+  print('Task 3 complete with $task2data');
 }
